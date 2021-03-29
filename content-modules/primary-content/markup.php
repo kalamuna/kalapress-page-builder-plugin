c1963780861e4e19cb0d9926b3d134f65b68f5f0
Jared Deschner <jared.deschner@gmail.com>
2021-03-28 22:08:43 -0600

    A few updates to core modules (primary content)

--- content-modules/primary-content/markup.php
+++ content-modules/primary-content/markup.php
@@ -1,9 +1,10 @@
+    <div class="row">
     <?php
         if( have_rows('columns') ) :
             while ( have_rows('columns') ) :
                 the_row();
         ?>
-            <div class="columns__content-column <?php echo ( get_sub_field('width') != 'auto' ? ' kpb-columns__content-column--'.get_sub_field('width') : '' ); ?><?php echo ( get_sub_field('bottom_padding') != 'none' ? ' kpb-columns__content-column--padding-bottom-'.get_sub_field('bottom_padding') : '' ); ?><?php echo ( get_sub_field('bottom_top') != 'none' ? ' kpb-columns__content-column--padding-top-'.get_sub_field('top_padding') : '' ); ?>">
+            <div class="col-md-6 <?php echo ( get_sub_field('width') != 'auto' ? '-md-'.get_sub_field('width') : '' ); ?><?php echo ( get_sub_field('bottom_padding') != 'none' ? ' kpb-columns__content-column--padding-bottom-'.get_sub_field('bottom_padding') : '' ); ?><?php echo ( get_sub_field('bottom_top') != 'none' ? ' kpb-columns__content-column--padding-top-'.get_sub_field('top_padding') : '' ); ?>">
                 <?php
 
                 switch (get_sub_field('content_type')):
@@ -31,3 +32,4 @@
             endwhile;
         endif;
     ?>
+    </div>
